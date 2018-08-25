bb6d5d247e01abdad38fc99c4e50c52c6a08656a
Nicole Nisters <n.nisters@gmail.com>
2018-08-26 00:30:40 +0200

    Use unittest for testing

--- tests/test_domaintool.py
+++ tests/test_domaintool.py
@@ -1,41 +1,49 @@
 """Test the domain name tool."""
+import unittest
+from unittest.mock import patch
+from io import StringIO
 import subprocess
-from domaintool import DomainChecker
+from domaintool.domaintool import DomainChecker
 
-def test_get_domains():
-    """Test the get_domains method."""
-    domain_checker = DomainChecker(3, 4, 'io', delay=1.5)
-    domains = domain_checker.get_domains()
-    assert domains != []
-
-def test_check_domain():
-    """Test the check_domain method."""
-    domain_checker = DomainChecker(3, 4, 'io', delay=1.5)
-    assert domain_checker.check_domain('ado.be') == 'not_available'
-    assert domain_checker.check_domain('jhgtyasjkhjdffd.be') == 'available'
-
-def test_check_domains():
-    """Test the check_domains method."""
-    domain_checker = DomainChecker(13, 13, 'be', delay=0.1)
-    domains = domain_checker.get_domains()
-    domain_checker.check_domains(domains)
-    assert True
-
-def test_cli():
-    """Test the cli."""
-    process = subprocess.run(['python3 domaintool.py -h'],
-                             shell=True,
-                             timeout=10,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, check=True)
-    output = process.stdout.split(b'\n')
-    assert output[0] == b'usage: domaintool.py [-h] [-f FILE] ' +\
-                        b'[-d DELAY] min max tld'
-
-    process = subprocess.run(['python3 domaintool.py 13 13 be -d 0.1'],
-                             shell=True,
-                             timeout=10,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, check=True)
-    output = process.stdout.split(b'\n')
-    assert output[0] == b'Checking 7 domains...'
+class DomaintoolTest(unittest.TestCase):
+    def test_get_domains(self):
+        """Test the get_domains method."""
+        domain_checker = DomainChecker(3, 4, 'io', delay=1.5)
+        domains = domain_checker.get_domains()
+        assert domains != []
+    
+    def test_check_domain(self):
+        """Test the check_domain method."""
+        domain_checker = DomainChecker(3, 4, 'io', delay=1.5)
+        assert domain_checker.check_domain('ado.be') == 'not_available'
+        assert domain_checker.check_domain('jhgtyasjkhjdffd.be') == 'available'
+    
+    def test_check_domains(self):
+        """Test the check_domains method."""
+        with patch('sys.stdout', new=StringIO()) as output:
+            domain_checker = DomainChecker(13, 13, 'be', delay=0.1)
+            domains = domain_checker.get_domains()
+            domain_checker.check_domains(domains)
+            output = output.getvalue().split('\n')
+            assert output[0] == 'Checking 7 domains...'
+    
+    def test_cli(self):
+        """Test the cli."""
+        with patch('sys.stdout', new=StringIO()) as output:
+            process = subprocess.run(['python3 domaintool/domaintool.py -h'],
+                                     shell=True,
+                                     timeout=10,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE, check=True)
+            assert process.returncode == 0
+            process_output = process.stdout.split(b'\n')
+            assert process_output[0] == b'usage: domaintool.py [-h] [-f FILE] [-d DELAY] min max tld'
+    
+            process = subprocess.run(['python3 domaintool/domaintool.py 13 13 be -d 0.1'],
+                                     shell=True,
+                                     timeout=10,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE, check=True)
+            assert process.returncode == 0
+            process_output = process.stdout.split(b'\n')
+            assert process_output[0] == b'Checking 7 domains...'
