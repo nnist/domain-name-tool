c61f33a7566bbbd5dec7726bdb906af1aedfdb09
Nicole Nisters <n.nisters@gmail.com>
2018-08-31 22:11:44 +0200

    Use test setUp for common functions

--- tests/test_domaintool.py
+++ tests/test_domaintool.py
@@ -11,37 +11,36 @@ logger = log.getLogger()
 logger.level = log.DEBUG
 
 class DomaintoolTest(unittest.TestCase):
+    def setUp(self):
+        self.dc = DomainChecker(3, 4, delay=1.5)
+
     def test_check_domain(self):
         """Test the check_domain method."""
-        domain_checker = DomainChecker(3, 4, delay=1.5)
-        assert domain_checker.check_domain('ado.be') == 'not_available'
-        assert domain_checker.check_domain('jhgtyasjkhjdffd.be') == 'available'
+        assert self.dc.check_domain('ado.be') == 'not_available'
+        assert self.dc.check_domain('jhgtyasjkhjdffd.be') == 'available'
     
     def test_check_domains_returns_correct_results(self):
-        domain_checker = DomainChecker(3, 4, delay=1.5)
-
         domains = ['ab.be', 'ado.be', 'foredescri.be']
-        results = domain_checker.check_domains(domains)
+        results = self.dc.check_domains(domains)
         expected_results = [('ab.be', 'not_available'),
                            ('ado.be', 'not_available'),
                            ('foredescri.be', 'available')]
         assert results == expected_results
     
     def test_get_tld_list_returns_list_of_proper_length(self):
-        domain_checker = DomainChecker(3, 4, delay=1.5)
-        tld_list = domain_checker.get_tld_list()
+        tld_list = self.dc.get_tld_list()
         # 1541 TLDs in total at 27 aug 2018. This number will only increase,
         # so testing for at least 1500 should be safe.
         assert len(tld_list) > 1500
 
     def test_get_domains_returns_correct_domains(self):
-        domain_checker = DomainChecker(1, 20, delay=1.5)
-        tld_list = domain_checker.get_tld_list()
-        domains = domain_checker.get_domains('sadnessparty', tld_list)
+        dc = DomainChecker(1, 20, delay=1.5)
+        tld_list = dc.get_tld_list()
+        domains = dc.get_domains('sadnessparty', tld_list)
         assert domains == ['sadness.party']
-        domains = domain_checker.get_domains('cathode', tld_list)
+        domains = dc.get_domains('cathode', tld_list)
         assert domains == ['catho.de']
-        domains = domain_checker.get_domains('aerobe', tld_list)
+        domains = dc.get_domains('aerobe', tld_list)
         assert domains == ['aero.be']
 
     def test_cli_help_prints_correct_output(self):
